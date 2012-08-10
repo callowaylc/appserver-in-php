2c3b563b4f99c14e48542e6d8a8b041bbbdef4ef
zalgo <zalgo@ub12.(none)>
2012-08-10 10:51:26 -0400

    Added comments to AiP::Middleware::ModPHP

--- AiP/Middleware/ModPHP.php
+++ AiP/Middleware/ModPHP.php
@@ -12,24 +12,31 @@ class ModPHP extends AiP\Middleware {
 
 	public function __invoke($context) {
 		
+		// PRE-REQUEST
 		// place "superglobals" into global scope
 		foreach(ModPHP::$globals as $global) {
 			$GLOBALS[$global] = array();
 			$context[$global] = &$GLOBALS[$global];
 		}	
 		
+		// PASS TO NEXT MIDDLEWARE INSTANCE
 		$result = call_user_func($this->application, $context);
 		
-		// unset globals
+		// POST-REQUEST
+		// unset globals - technically we dont have to unset our 
+		// reference, but doing so for clarity sake and isset
+		// funciton, should that ever come into play
 		foreach(ModPHP::$globals as $global) {
 			unset($GLOBALS[$global]);
+			unset($context[$global]);
 		}	
 		
+		// RESPONSE W/HEADERS
 		return $result;
 	
 	}
 
 	private static $globals = [
-		'_GET', '_POST', '_PUT', '_DELETE', '_COOKIE', '_FILES'
+		'_GET', '_POST', '_PUT', '_DELETE', '_COOKIE', '_FILES', '_SESSION'
 	];
 }
